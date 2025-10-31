c6b8aaf3897f91071ecc8ab204a39209a5d6d2c7
tiesen243 <ttien56906@gmail.com>
2025-10-31 23:50:08 +0700

    chore: minor changes

--- src/Abstract/Entity.php
+++ src/Abstract/Entity.php
@@ -4,8 +4,5 @@ namespace Core\Abstract;
 
 abstract class Entity
 {
-    public static string $tableName;
-    public static string $primaryKey;
-
     abstract public function validate(): array;
 }
